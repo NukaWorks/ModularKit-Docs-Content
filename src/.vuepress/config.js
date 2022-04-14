b20c39a0d3aefc69b05c5e836dba12b99d1cab5b
Powerm1nt <sundev79@nutdevs.org>
2022-04-14 12:31:03 +0200

    Updated module config

---
+++ src/.vuepress/config.js
@@ -0,0 +1,58 @@
+const {description} = require('../../package')
+
+module.exports = {
+    title: 'ModularKit Documentation',
+    description: description,
+    head: [
+        ['meta', {name: 'theme-color', content: '#2196f3'}],
+        ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
+        ['meta', {name: 'apple-mobile-web-app-status-bar-style', content: 'black'}]
+    ],
+    theme: 'yuu',
+    themeConfig: {
+        yuu: {
+            defaultDarkTheme: true,
+            defaultColorTheme: 'blue',
+            disableThemeIgnore: true,
+            colorThemes: ['blue'],
+            labels: {
+                darkTheme: 'Dark Mode'
+            }
+        },
+        repo: '',
+        editLinks: false,
+        docsDir: '',
+        editLinkText: '',
+        lastUpdated: true,
+        nav: [
+            {
+                text: 'Guide',
+                link: '/guide/',
+            },
+            {
+                text: 'Config',
+                link: '/config/'
+            },
+            {
+                text: 'Github',
+                link: 'https://github.com/NukaWorks/ModularKit'
+            }
+        ],
+        sidebar: {
+            '/guide/': [
+                {
+                    title: 'Guide',
+                    collapsable: false,
+                    children: [
+                        'using-vue',
+                    ]
+                }
+            ],
+        }
+    },
+
+    plugins: [
+        '@vuepress/plugin-back-to-top',
+        '@vuepress/plugin-medium-zoom',
+    ]
+}
